9f418dd9c1c53fce56083b67da88324c651c02c5
Roland Starke <rolandstarke@users.noreply.github.com>
2026-01-14 21:33:26 +0100

    stuff

---
+++ capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'com.example.app',
+  appName: 'farm-diary2',
+  webDir: 'dist'
+};
+
+export default config;
