498ff0a9945c0443f8c5b798905c1af50833adb7
Roland Starke <rolandstarke@users.noreply.github.com>
2026-01-16 22:46:08 +0100

    icon

--- vite.config.mjs
+++ vite.config.mjs
@@ -58,12 +58,6 @@ export default defineConfig({
             "src": "pwa-512x512.png",
             "sizes": "512x512",
             "type": "image/png"
-          },
-          {
-            "src": "maskable-icon-512x512.png",
-            "sizes": "512x512",
-            "type": "image/png",
-            "purpose": "maskable"
           }
         ],
       },
